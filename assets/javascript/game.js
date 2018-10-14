2cea480167c6ea90e4b4e7362da0ae602295e99f
quotient group <soybeena@icloud.com>
2018-10-13 22:28:57 -0700

    mostly finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,82 @@
 var randomNumber;
-var losses;
-var wins;
+var losses = 0;
+var wins = 0;
+var data = 0;
 
-for(var 1=0; i < 4; i++){
+var restarting = function (){
+    
+    $(".crystals").empty();
+
+    var images = [
+                    'unit-4-game\assets\images\images\blue.png',
+                    'unit-4-game\assets\images\images\green.png',
+                    'unit-4-game\assets\images\images\red.png',
+                    'unit-4-game\assets\images\images\yellow.png'];
+    
+    randomNumber = Math.random() * 69 ) + 30;
+
+    $("#number" .html('Random Number: ' + randomNumber);
+
+    for(var 1=0; i < 4; i++)    {
 
     var randomnity = Math.floor(Math.random() * 12);
     var crystalGroup = $("<div>");
-        crystalGroup.attr({
-            "class": 'crystalgroup',
-            "numbers": randomnity
-        });
+            crystalGroup.attr({
+                "class": 'crystalgroup',
+                "numbers": randomnity
+            });
+            crystalgroup.css({
+                "background-image":"url( '" + images[i] + "')",
+                "background-size":"cover"
+            });
 
-    $(".crystals").append(crystalgroup);
+        $(".crystals").append(crystalgroup);
 
-}
+    }
+
+    $("#data").html("Scoring: " + data)
+
+}   
+    
+restarting();
+
+
+
+$(document).on('click', ".crystalGroup", function () {
+
+    
+    var num = parsing($(this).attr('numbers'));
+
+    data += num;
+
+    
+    $("#data") .html("Scoring: " + data);
+    
+    console.log(data);
+
+    if(data > randomNumber){
+        
+        losses++;
+
+        $("#losses").html("Player losses: " + losses);
+
+        data = 0;
+
+        restarting();
+
+    }
+    else if(data === randomNumber){
+
+        wins++;
+
+        $("#wins") .html("Player wins: " + wins);
+    
+        data = 0;
+
+        restarting();
+    
+    
+    }
+    
+});
+    
